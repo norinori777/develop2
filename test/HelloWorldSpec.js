b8d05c09eee1b26da6eb4ea029b65b00de90a1f2
norinori6791 <norinori6791@gmail.com>
2017-01-15 18:49:02 +0900

    containerとcomponentに分離
    HelloWorldコンポーネントをcontainerとcomponentにファイルを分離させた。

--- test/HelloWorldSpec.js
+++ test/HelloWorldSpec.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
-import HelloWorld from '../develop/assets/js/jsx/HelloWorld.js'
+import {connect,Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux'
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
+import rootReducer from '../develop/assets/js/redux/reducers/main.js'
+import HelloWorld from '../develop/assets/js/jsx/components/HelloWorld.js'
 
 describe("HelloWorld",function(){
 	let myComponent
+
+	const store = createStore(combineReducers({
+		rootReducer,
+		routing: routerReducer
+	}))
 	
 	it('should render',function(){
-		myComponent = TestUtils.renderIntoDocument(<HelloWorld name='test'></HelloWorld>)
-		expect(ReactDOM.findDOMNode(myComponent)).toBeDefined()
-		//expect(ReactDOM.findDOMNode(myComponent.props.title)).toBe("test")
+		//myComponent = TestUtils.renderIntoDocument(<Provider store={store}>
+		//												<HelloWorld name="test"></HelloWorld>
+		//											</Provider>)
+		//expect(ReactDOM.findDOMNode(myComponent)).toBeDefined()
+		//console.log(myComponent.props.rootReducer)
+		//expect(ReactDOM.findDOMNode(myComponent.props.rootReducer.name)).toBe("NORI")
 	});
 });
